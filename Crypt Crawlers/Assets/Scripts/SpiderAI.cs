a3a028e33e9fe821e408b070b4d6085f9f892749
EvanDyson <evandyson@ufl.edu>
2024-02-20 17:12:41 -0500

    updated spiderAI

--- Crypt Crawlers/Assets/Scripts/SpiderAI.cs
+++ Crypt Crawlers/Assets/Scripts/SpiderAI.cs
@@ -5,36 +5,71 @@ using UnityEngine;
 public class SpiderAI : MonoBehaviour
 {
     public GameObject player;
-    public Transform playerT;
+    public List<Transform> points;
     public float speed;
     public float chaseDistance;
     //public Animator animator;
-
-    //private Animator animation;
     private Rigidbody2D rb;
+    //private Animator animation;
     private float playerDistance;
-    //public AudioSource SkeleFootsteps;
+
+    Transform nextPoint;
+    int pointNum = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //SkeleFootsteps.enabled = true;
+        nextPoint = points[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         playerDistance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        // use rotation for spider to angle it towards player
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         if (playerDistance < chaseDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            facePlayer();
         }
+        else
+        {
+            Vector2 directionToGo = (nextPoint.position - transform.position).normalized;
+            float distanceToPoint = Vector2.Distance(nextPoint.position, transform.position);
+
+            // needs (* Time.deltaTime)? VVV
+            rb.velocity = directionToGo * speed;
+            if (distanceToPoint <= 0.5f)
+            {
+                pointNum++;
+                if (pointNum >= points.Count)
+                {
+                    pointNum = 0;
+                }
+                nextPoint = points[pointNum];
+            }
+        }
+    }
+
+    void facePlayer()
+    {
+        Vector3 vectorToTarget = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 10);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Gizmos.DrawWireSphere(points[i].position, 0.5f);
+        }
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            Gizmos.DrawLine(points[i].position, points[i + 1].position);
+        }
+        Gizmos.DrawLine(points[points.Count-1].position, points[0].position);
     }
 }
