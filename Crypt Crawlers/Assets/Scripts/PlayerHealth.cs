d887ce1df4b2ff727799a649c6916cb0797d29e7
EvanDyson <evandyson@ufl.edu>
2024-04-06 15:28:45 -0400

    added back in Alex's ammo spawner and web slowness

--- Crypt Crawlers/Assets/Scripts/PlayerHealth.cs
+++ Crypt Crawlers/Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    private PlayerMovement playerMovement;
     public float health;
     public float maxHealth;
-    public Image HealthBar;
+    public UnityEngine.UI.Image HealthBar;
+    public UnityEngine.UI.Image HealthBarOutline;
+    public Sprite FirstHealthBarOutline;
+    public Sprite SecondHealthBarOutline;
+    private float playerSpeed;
     [SerializeField] private Transform respawnPoint; // drag respawn point here in inspector
+    private bool imageChanged;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerMovement = GetComponent<PlayerMovement>();
+        playerSpeed = playerMovement.moveSpeed;
         maxHealth = health;
+        imageChanged = false;
     }
 
     // Update is called once per frame
@@ -25,19 +35,43 @@ public class PlayerHealth : MonoBehaviour
             Respawn();
 
         HealthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+        if (health <= 10 && imageChanged == false)
+        {
+            HealthBarOutline.sprite = SecondHealthBarOutline;
+            imageChanged = true;
+        }
+        else if (health > 10 && imageChanged == true)
+        {
+            HealthBarOutline.sprite = FirstHealthBarOutline;
+            imageChanged = false;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("deathline")) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "shooting web(Clone)")
+        {
+            StartCoroutine(SlowDown(2.0f));
+        }
+        if (collision.CompareTag("deathline"))
+        {
             Respawn();
         }
     }
 
-    private void Respawn() {
+    IEnumerator SlowDown(float duration)
+    {
+        playerMovement.moveSpeed = 1.5f;
+        yield return new WaitForSeconds(duration);
+        playerMovement.moveSpeed = playerSpeed;
+    }
+
+    private void Respawn()
+    {
         // reset player position to respawn point
         transform.position = respawnPoint.position;
-       
+        playerMovement.moveSpeed = playerSpeed;
         health = maxHealth;
-        
+
     }
 }
