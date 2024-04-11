7d4e30d934c1b2ad68019bf93640cd1ff207fda7
Alex F <alexfleisalex@gmail.com>
2024-04-10 20:54:15 -0400

    Bug Fixes

--- Crypt Crawlers/Assets/Scripts/Dialogue.cs
+++ Crypt Crawlers/Assets/Scripts/Dialogue.cs
@@ -19,7 +19,7 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) {
+        if(Input.GetKeyUp(KeyCode.F)) {
             if (textComponent.text == lines[index]) {
                 NextLine();
             }
